ac902db6925f8079e3840a68d1ff3425c22798df
kmr-sahil <91898837+kmr-sahil@users.noreply.github.com>
2023-05-11 20:20:35 +0530

    Add files via upload

---
+++ WLDD - React - clone/src/App.js
@@ -0,0 +1,74 @@
+
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+
+  const [mode, setmode] = useState('light')
+
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+    setTimeout( () =>{
+      setAlert(null)
+    },2000)
+  }
+
+  const removeCls = () => {
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-primary')
+  }
+
+let toggleMode = (cls) => {
+  removeCls();
+    document.body.classList.add('bg-'+cls)
+    if(mode == 'light'){
+        document.body.style.backgroundColor = 'grey';
+        document.body.style.color = 'white';
+        setmode('dark');
+        showAlert("Dark mode has been enabled", "success")
+    }
+    else{
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'dark';
+      setmode('light');
+      showAlert("Light mode has been enabled", "success")
+    }
+}
+
+  return (
+    <>
+    <Router> 
+    <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    
+    <div className="container">
+      <Routes>
+        <Route exact path="/about" element={<About mode={mode}/>}/>
+        <Route exact path="/" element={<Textform heading="Enter the text" mode={mode} showAlert={showAlert}/>}/>
+      </Routes>
+  
+    </div>
+    </Router>
+    </>
+   
+  );
+}
+
+export default App;
+
---
+++ WLDD - React - clone/src/components/Textform.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+export default function Textform(props) {
+
+    const handleCopy= () => {
+        navigator.clipboard.writeText(text)
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to clipboard", "success")
+    }
+
+    const handleUpclick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Upper Case Successful", "success")
+    }
+    const handleLoclick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Lower Case Successful", "success")
+    }
+
+    const handleOnchange = (event) => {
+        setText(event.target.value)
+    }
+    const [text, setText] = useState('')
+
+    const clearText = () => {
+        setText('')
+        props.showAlert("Text Cleared", "success")
+    }
+
+  return (
+    <>
+    <div className="container my-2" style={{color: props.mode==='dark'?'white':'black'}} >
+        <h1>{props.heading}</h1>
+    <div className="mb-3">
+        <textarea  className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"
+        style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}}></textarea>
+    </div>
+    <button disabled={text.length===0} className='btn btn-primary mx-1' onClick={handleUpclick}>Convert to Uppercase</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-1' onClick={handleLoclick}>Convert to Lowercase</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-1' onClick={clearText}>Clear Text</button>
+    <button disabled={text.length===0} className='btn btn-primary mx-1' onClick={handleCopy}>Copy Text</button>
+    
+    
+    <div  className="conatiner mx-2 my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(/\s+/).length - 1} words & {text.length} charachters </p>
+    </div>
+    
+    </div>
+    </>
+  
+  )
+}
