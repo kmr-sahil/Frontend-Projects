8c08e63f7c963cd60970ad35715900837b41b527
kmr-sahil <91898837+kmr-sahil@users.noreply.github.com>
2023-06-09 23:20:36 +0530

    Update script.js
    added features similar to iphone calculator

--- Calculator /dist/script.js
+++ Calculator /dist/script.js
@@ -1,21 +1,46 @@
 let screen = document.querySelector('#output')
 let numbers = document.querySelectorAll(".btn-primary, .btn-secondary")
-
+var cal = ''
+var display = ''
+var operator = ''
 
 Array.from(numbers).forEach((button) => {
     button.addEventListener('click', (e) => {
         
         if(e.target.innerText === 'AC'){
             screen.innerText = '0'
+            cal = ''
+            display = ''
         }
         else if(e.target.innerText === '='){
-            console.log(screen.innerText)
-            screen.innerText = eval(screen.innerText)
+            console.log(cal)
+            screen.innerText = eval(cal)
+            cal = screen.innerText
+            display = screen.innerText
         }
         else{
-            
-            e.target.className === 'btn-primary'? screen.innerText += e.target.innerText : screen.innerText += e.target.value;
-        }
+            cal = cal + (e.target.className === 'btn-primary'? e.target.innerText : e.target.value)
+        
+            if (e.target.className === 'btn-primary' && operator !== '' ){
+                display = e.target.innerText
+                screen.innerHTML = display
+                operator = ''
+                console.log("no")
+            }
 
+            else if(e.target.className === 'btn-primary' || e.target.innerText === '0'){
+                display +=  e.target.innerText
+                screen.innerHTML = display
+                console.log("pri")
+            }
+            
+            else {
+                operator = e.target.value
+                screen.innerHTML = display
+                console.log("op")
+            }
+            console.log(display)
+            console.log(cal)
+        }
     })
 })
